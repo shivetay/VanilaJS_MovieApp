ed063a84f2a26a5507c81f5a7ad270a09cb67fbf
Łukasz Dawidowicz <lddev@outlook.com>
2021-03-09 14:38:26 +0100

    add pagination

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import { getMovies } from './scripts/server_methods';
+import { getMovies, changePage } from './scripts/server_methods';
 
 import './main.scss';
 
@@ -15,45 +15,9 @@ const app = {
     thisApp.btn = document.querySelectorAll('.btn');
 
     for (let btn of this.btn) {
-      btn.addEventListener('click', this.changePage);
+      btn.addEventListener('click', changePage);
     }
   },
-
-  changePage: function (e) {
-    e.preventDefault();
-    const thisApp = this;
-    thisApp.currentPage = 1;
-    thisApp.first = document.getElementById('first');
-    thisApp.back = document.getElementById('back');
-    thisApp.next = document.getElementById('next');
-    console.log(thisApp.currentPage);
-
-    if (e.target.innerHTML == 'First') {
-      thisApp.currentPage = 1;
-      // getMovies(currentPage);
-      this.loadMovies(thisApp.currentPage);
-      console.log(thisApp.currentPage);
-    } else if (e.target.innerHTML == 'Next') {
-      thisApp.currentPage++;
-      // getMovies(currentPage);
-      this.loadMovies(thisApp.currentPage);
-      console.log(thisApp.currentPage);
-    } else {
-      thisApp.currentPage--;
-      // getMovies(currentPage);
-      this.loadMovies(thisApp.currentPage);
-      console.log(thisApp.currentPage);
-    }
-
-    if (thisApp.currentPage === 1 || thisApp.currentPage <= 0) {
-      thisApp.first.disabled = true;
-    }
-    console.log(thisApp.currentPage);
-
-    return thisApp.currentPage;
-  },
-
-  currentPage: function () {},
 
   /* app init */
   init: function () {
--- src/scripts/server_methods.js
+++ src/scripts/server_methods.js
@@ -1,6 +1,7 @@
 import UI from './UI';
 
 const key = '3587a3fa0e0740b636671ff686bf592b';
+let currentPage = 1;
 
 export const getMovies = async (page) => {
   try {
@@ -18,4 +19,36 @@ export const getMovies = async (page) => {
   } catch (err) {
     throw new Error('error');
   }
+};
+
+export const changePage = (e) => {
+  e.preventDefault();
+
+  // currentPage = 1;
+  const first = document.getElementById('first');
+  const back = document.getElementById('back');
+  // const next = document.getElementById('next');
+  console.log(currentPage);
+
+  if (e.target.innerHTML == 'First') {
+    currentPage = 1;
+    getMovies(currentPage);
+    console.log(currentPage);
+  } else if (e.target.innerHTML == 'Next') {
+    currentPage++;
+    getMovies(currentPage);
+  } else {
+    currentPage--;
+    getMovies(currentPage);
+  }
+
+  if (currentPage === 1 || currentPage <= 0 || currentPage === 'undefined') {
+    first.disabled = true;
+    back.disabled = true;
+    currentPage = 1;
+  } else {
+    first.disabled = false;
+    back.disabled = false;
+  }
+  console.log(currentPage);
 };
